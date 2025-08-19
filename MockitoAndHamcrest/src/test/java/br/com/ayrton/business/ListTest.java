7e623ed94aa6451f98cd5b428096fbd6c18c1f29
Ayrton Yoshii <itiro_yoshii@hotmail.com>
2025-08-19 11:25:01 -0300

    add CourseBusinessMockTest and ListTest with mock examples; rename CourseBusinessTest to CourseBusinessStubTest

---
+++ MockitoAndHamcrest/src/test/java/br/com/ayrton/business/ListTest.java
@@ -0,0 +1,59 @@
+package br.com.ayrton.business;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class ListTest {
+
+    @Test
+    void testMockingList_WhenSizeIsCalled_ShouldReturn10(){
+        // Given
+        List list = mock(List.class);
+        when(list.size()).thenReturn(10);
+
+        // When & Then
+        assertEquals(10, list.size(), () -> "The mocked list size should be 10!");
+    }
+
+    @Test
+    void testMockingList_WhenSizeIsCalled_ShouldReturnMultipleValues(){
+        // Given
+        List list = mock(List.class);
+        when(list.size()).thenReturn(10).thenReturn(20).thenReturn(30);
+
+        // When & Then
+        assertEquals(10, list.size(), () -> "The mocked list size should be 10!");
+        assertEquals(20, list.size(), () -> "The mocked list size should be 10!");
+        assertEquals(30, list.size(), () -> "The mocked list size should be 10!");
+    }
+
+    @Test
+    void testMockingList_WhenGetIsCalled_ShouldReturnAyrton(){
+        // Given
+        List list = mock(List.class);
+        when(list.get(0)).thenReturn("Ayrton");
+
+        // When & Then
+        assertEquals("Ayrton", list.get(0), () -> "The mocked list should return 'Ayrton' when get(0) is called!");
+        assertNull(list.get(1), () -> "The mocked list should return null when get(1) is called!");
+    }
+
+    @Test
+    void testMockingList_WhenGetIsCalledWithArgumentMatcher_ShouldReturnAyrton(){
+        // Given
+        List list = mock(List.class);
+
+        // If you are using argument matchers, all arguments are provided by matchers
+        when(list.get(anyInt())).thenReturn("Ayrton");
+
+        // When & Then
+        assertEquals("Ayrton", list.get(anyInt()), () -> "The mocked list should return 'Ayrton' when get(anyInt()) is called!");
+        assertEquals("Ayrton", list.get(0), () -> "The mocked list should return 'Ayrton' when get(0) is called!");
+        assertEquals("Ayrton", list.get(1), () -> "The mocked list should return 'Ayrton' when get(0) is called!");
+//      assertNull(list.get(1), () -> "The mocked list should return null when get(1) is called!");
+    }
+}
