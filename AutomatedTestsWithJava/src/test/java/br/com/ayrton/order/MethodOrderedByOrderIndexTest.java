263871e2ee5fed37edcae58974708ef9b09ce016
Ayrton Yoshii <itiro_yoshii@hotmail.com>
2025-08-18 21:54:08 -0300

    rename MethodOrderedByOrderTest to MethodOrderedByOrderIndexTest and enhance test output with afterEach method

--- AutomatedTestsWithJava/src/test/java/br/com/ayrton/order/MethodOrderedByOrderIndexTest.java
+++ AutomatedTestsWithJava/src/test/java/br/com/ayrton/order/MethodOrderedByOrderIndexTest.java
@@ -1,36 +1,44 @@
 package br.com.ayrton.order;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
+//@Order(3)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@Order(3)
-public class MethodOrderedByOrderTest {
+public class MethodOrderedByOrderIndexTest {
+    StringBuilder builder = new StringBuilder();
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("The actual value is: " + builder);
+    }
 
     @Test
     @Order(4)
     void testC(){
         System.out.println("Test C");
+        builder.append("4");
     }
 
     @Test
     @Order(2)
     void testA(){
         System.out.println("Test A");
+        builder.append("2");
     }
 
     @Test
     @Order(3)
     void testD(){
         System.out.println("Test D");
+        builder.append("3");
     }
 
     @Test
     @Order(1)
     void testB(){
         System.out.println("Test B");
+        builder.append("1");
     }
 
 
