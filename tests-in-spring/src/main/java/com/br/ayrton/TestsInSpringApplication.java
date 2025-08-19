1be715fd5d5bdceeda35570956b3f8908d3396bb
Ayrton Yoshii <itiro_yoshii@hotmail.com>
2025-08-19 19:57:39 -0300

    initialize Spring Boot project with basic structure and configuration; add .gitignore, application properties, and initial test classes

---
+++ tests-in-spring/src/main/java/com/br/ayrton/TestsInSpringApplication.java
@@ -0,0 +1,13 @@
+package com.br.ayrton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsInSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsInSpringApplication.class, args);
+	}
+
+}
---
+++ tests-in-spring/src/test/java/com/br/ayrton/TestsInSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ayrton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsInSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
